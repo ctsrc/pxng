2a71a19ec26c3146167c19eae9d7e40732713c5a
Erik Nordstrøm <erik@nordstroem.no>
2023-06-28 18:17:20 +0200

    wip

--- Pxng/Pxng/ContentView.swift
+++ Pxng/Pxng/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     // Acceleration of Player 1 paddle Y coordinate, in range [-0.5, 0.5].
     @State var p1YAcceleration = 0.0;
+    // Timestamp of last update of Player 1 paddle Y acceleration
+    @State var p1YAccelUpdateTimestamp: Optional<Date> = nil;
     // Velocity of Player 1 paddle Y coordinate, in range [-0.5, 0.5].
     @State var p1YVelocity = 0.0;
     // Offset of Player 1 paddle Y coordinate, in range [-0.5, 0.5].
@@ -17,6 +19,8 @@ struct ContentView: View {
     
     // Acceleration of Player 2 paddle Y coordinate, in range [-0.5, 0.5].
     @State var p2YAcceleration = 0.0;
+    // Timestamp of last update of Player 2 paddle Y acceleration
+    @State var p2YAccelUpdateTimestamp: Optional<Date> = nil;
     // Velocity of Player 2 paddle Y coordinate, in range [-0.5, 0.5].
     @State var p2YVelocity = 0.0;
     // Offset of Player 2 paddle Y coordinate, in range [-0.5, 0.5].
@@ -43,8 +47,17 @@ struct ContentView: View {
                 .opacity(0.1)
                 .gesture(DragGesture()
                     .onChanged({ value in
-                        p1YAcceleration = (value.location.y / viewHeight) - 0.5;
-                        print("P1 drag Y acceleration \(p1YAcceleration)")
+                        if let p1YAccelUpdateTimestampPrev = p1YAccelUpdateTimestamp {
+                            print("P1 drag gesture \(value)")
+                            print("P1 drag gesture time \(value.time)")
+                            let timedelta = value.time.timeIntervalSinceReferenceDate - p1YAccelUpdateTimestampPrev.timeIntervalSinceReferenceDate;
+                            print("timedelta \(timedelta)")
+                            p1YAcceleration = (value.location.y / viewHeight) - 0.5;
+                            print("P1 drag Y acceleration \(p1YAcceleration)")
+                            p1YVelocity += p1YAcceleration * timedelta;
+                            print("P1 Y velocity \(p1YVelocity)")
+                        }
+                        p1YAccelUpdateTimestamp = value.time;
                     })
                     .onEnded({ _ in
                         p1YAcceleration = 0.0;
@@ -60,8 +73,17 @@ struct ContentView: View {
                 .opacity(0.1)
                 .simultaneousGesture(DragGesture()
                     .onChanged({ value in
-                        p2YAcceleration = (value.location.y / viewHeight) - 0.5;
-                        print("P2 drag Y acceleration \(p2YAcceleration)")
+                        if let p2YAccelUpdateTimestampPrev = p2YAccelUpdateTimestamp {
+                            print("P2 drag gesture \(value)")
+                            print("P2 drag gesture time \(value.time)")
+                            let timedelta = value.time.timeIntervalSinceReferenceDate - p2YAccelUpdateTimestampPrev.timeIntervalSinceReferenceDate;
+                            print("timedelta \(timedelta)")
+                            p2YAcceleration = (value.location.y / viewHeight) - 0.5;
+                            print("P2 drag Y acceleration \(p2YAcceleration)")
+                            p2YVelocity += p2YAcceleration * timedelta;
+                            print("P2 Y velocity \(p2YVelocity)")
+                        }
+                        p2YAccelUpdateTimestamp = value.time;
                     })
                     .onEnded({ _ in
                         p2YAcceleration = 0.0;
