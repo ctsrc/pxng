b5c37694b517d22a01539e7b116ce7b910ed06a4
Erik Nordstrøm <erik@nordstroem.no>
2023-06-28 16:45:57 +0200

    Initial commit.

---
+++ Pxng/Pxng/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Pxng
+//
+//  Created by Erik N on 28/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Rectangle().frame(width: 10, height: 100, alignment: .center)
+        Circle().frame(width: 25, height: 25, alignment: .center)
+        Rectangle().frame(width: 10, height: 100, alignment: .center)
+    }
+}
---
+++ Pxng/Pxng/PxngApp.swift
@@ -0,0 +1,17 @@
+//
+//  PxngApp.swift
+//  Pxng
+//
+//  Created by Erik N on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PxngApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
